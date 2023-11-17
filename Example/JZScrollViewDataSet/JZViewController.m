93a3dac1a8482b742282f384c55cb8c8119a75fb
Jentle <jt.zhi@zuche.com>
2023-11-17 12:55:41 +0800

    初始化

---
+++ Example/JZScrollViewDataSet/JZViewController.m
@@ -0,0 +1,29 @@
+//
+//  JZViewController.m
+//  JZScrollViewDataSet
+//
+//  Created by Jentle on 11/17/2023.
+//  Copyright (c) 2023 Jentle. All rights reserved.
+//
+
+#import "JZViewController.h"
+
+@interface JZViewController ()
+
+@end
+
+@implementation JZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
